369e67755c57e9a7ff5ae4dd9f21e4d633711631
mariamopeyemi <opeyemi.afolabi45@gmail.com>
2021-07-27 11:01:02 +0100

    hamburger update

--- app.js
+++ app.js
@@ -1,20 +1,9 @@
-const btn = document.querySelector('.annoy-btn');
-const body = document.querySelector('#body');
 
-btn.addEventListener('mouseover', setTimeout(function() {
-    console.log('YOU MOUSED OVER ME!');
-
-    const height = Math.floor (Math.random() *window.innerHeight);
-    const width = Math.floor (Math.random() *window.innerWidth);
-
-    const r= Math.floor(Math.random()*256);
-    const g= Math.floor(Math.random()*256);
-    const b= Math.floor(Math.random()*256);
-    const color = `rgb(${r},${g},${b})`;
-   
-    btn.style.left = `${width}px`;
-    btn.style.top = `${height}px`;
-    console.log(body);
-    // body.style.backgroundColor= color;
-    body.style.backgroundColor= color;
-}, 2000));
+function myFunction() {
+    var x = document.getElementById("nav-bar");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
